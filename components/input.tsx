6be9ae396cd35afbdf1f2dd0660fb6217b5e24b1
Ian Hwang <meen813@hotmail.com>
2023-07-12 00:52:01 -0700

    fixed link & a tag lagacy error by adding legacyBehavior

--- components/input.tsx
+++ components/input.tsx
@@ -39,14 +39,14 @@ export default function Input({
                         className="appearance-none pl-7 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
                     />
                     <div className="absolute right-0 pointer-events-none pr-3 flex items-center">
-                        <span className="text-gray-500">KRW</span>
+                        <span className="text-gray-500">USD</span>
                     </div>
                 </div>
             ) : null}
             {kind === "phone" ? (
                 <div className="flex rounded-md shadow-sm">
                     <span className="flex items-center justify-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 select-none text-sm">
-                        +82
+                        +1
                     </span>
                     <input
                         id={name}
