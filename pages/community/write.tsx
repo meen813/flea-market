2ed63702cef785eb9a4294db4c87482adf9bf2d0
Ian Hwang <meen813@hotmail.com>
2023-09-09 17:06:42 -0700

    feat: two new models added to schema

--- pages/community/write.tsx
+++ pages/community/write.tsx
@@ -1,9 +1,18 @@
 import type { NextPage } from "next";
+import { useForm } from "react-hook-form";
 import Button from "../../components/button";
 import Layout from "../../components/layout";
 import TextArea from "../../components/textarea";
 
+interface WriteForm {
+    question: string;
+}
+
 const Write: NextPage = () => {
+    const { register, handleSubmit } = useForm<WriteForm>();
+    const onValid = (data: WriteForm) => {
+        console.log(data)
+    };
     return (
         <Layout canGoBack title="Write Post">
             <form className="p-4 space-y-4">
